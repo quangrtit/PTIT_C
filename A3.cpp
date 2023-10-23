8fe3fb3958faa2f8efe1a7c58c4e5ba45a35062d
QUANG <quangkhongvan4444@gmail.com>
2023-10-23 14:13:00 +0700

    change 1

--- A3.cpp
+++ A3.cpp
@@ -1,45 +1,6 @@
-#include<bits/stdc++.h>
-using namespace std;
-string chuanhoa(string s){
-	for(int i = 0;i<s.size();i++){
-		if(s[i] >=65 && s[i] <= 90){
-			s[i] = char(s[i]+32);
-		}
-	}
-	return s;
-}
-int main(){
-	int t;cin>>t;
-	string check = "meow";
-	//cin.ignore();
-	while(t--){
-		int n;cin>>n;
-		string s;cin>>s;
-		s = chuanhoa(s);
-		if(s.size() <4 || s[0] != 'm'){
-			cout<<"NO\n";
-		}
-		else{
-			vector<char> x(100);
-			x[0] = 'm';
-			int ii = 1;
-			for(int i = 0;i<s.size();i++){
-				if(s[i] != x[ii-1]){
-					x[ii] = s[i];
-					ii ++;
-				}
-			}
-			string s1 = "";
-			for(int i = 0;i<ii;i++){
-				s1 += x[i];
-			}
-			//cout<<s1<<endl;
-			if(s1 == check){
-				cout<<"YES"<<endl;
-			}
-			else{
-				cout<<"NO"<<endl;
-			}
-		}
-	}
+#include<iostream>
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
 }
